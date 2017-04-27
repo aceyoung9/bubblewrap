5302245323ae828d45494375216b73e86ae5f1c6
Alice Young <alice@ccs.neu.edu>
2017-04-27 16:29:50 -0400

    tweak so it work on bubble bubbles.

--- Trial1.py
+++ Trial1.py
@@ -5,18 +5,38 @@ import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")
 import cv2;
 import numpy as np;
 
-# Read image
-im = cv2.imread("test-images/blob.jpg", cv2.IMREAD_GRAYSCALE)
+# print cv2.__version__;
 
-# Set up the detector with default parameters.
-detector = cv2.SimpleBlobDetector()
+# Read image
+im = cv2.imread("test-images/still2.jpg", cv2.IMREAD_GRAYSCALE)
+im = cv2.bitwise_not(im)
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+params.maxThreshold = 200
+params.minThreshold = 0
+
+params.filterByArea = True
+params.minArea = 150
+
+params.filterByInertia = False
+# params.minInertiaRatio = 0.5
+#
+params.filterByColor = False
+params.minRepeatability = 0
+params.filterByCircularity = False
+# params.minCircularity = 0.85
+
+# Set up the detector
+detector = cv2.SimpleBlobDetector(params)
 
 # Detect blobs.
 keypoints = detector.detect(im)
 
 # Draw detected blobs as red circles.
 # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0, 0, 255),
+im_with_keypoints = cv2.drawKeypoints(cv2.bitwise_not(im), keypoints, np.array([]), (255, 180, 0),
                                       cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Show keypoints
